0db7de4db9c208e7dce251d703f6dbabdb2336df
Guillaume Turcat <gturcat@me.com>
2019-02-20 15:10:49 +0100

    bookings create new index_by_user"

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_133059) do
+ActiveRecord::Schema.define(version: 2019_02_20_115900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id"
+    t.bigint "flat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "title"
