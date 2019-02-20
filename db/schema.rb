7567439eae98464bd96c9519d3af08d200e9f27d
Guillaume Turcat <gturcat@me.com>
2019-02-20 16:26:50 +0100

    bookings create new index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_133059) do
+ActiveRecord::Schema.define(version: 2019_02_20_150711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id"
+    t.bigint "flat_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "title"
@@ -49,5 +60,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_133059) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
