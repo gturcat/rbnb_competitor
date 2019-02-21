429d3841ac791acabc171aedd618d5352e187277
Guillaume Turcat <gturcat@me.com>
2019-02-21 23:03:29 +0100

    filtre les flats en fonction de leur dispo

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,21 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+    @flats = @flats.near(params[:address], 10) if params[:address] != ""
+    @flats = @flats.where("capacity >= #{params[:capacity]}") if params[:capacity] != ""
+   #@flats = @flats.reject { |flat| not_available?(flat) }
+
+
+  @flat_to_locate = @flats.where.not(latitude: nil, longitude: nil)
+  @flat_to_locate = @flat_to_locate.reject { |flat| not_available?(flat) }
+  @flats = @flat_to_locate
+  @markers = @flat_to_locate.map do |flat|
+       {
+          lng: flat.longitude,
+          lat: flat.latitude
+        }
+      end
+  end
 
  def list
    @show_type = params[:city].nil? & params[:capacity].nil?
@@ -18,7 +35,7 @@ class FlatsController < ApplicationController
         lat: flat.latitude
       }
     end
- end
+  end
 
   def new
     @flat = Flat.new
@@ -40,5 +57,18 @@ class FlatsController < ApplicationController
     params.require(:flat).permit(:address, :title, :description, :capacity, :price, :photo)
   end
 
-end
+  def not_available?(flat)
+    available = 0
+    if (params[:start_date] != "" && params[:end_date] !="")
+    period = params[:start_date].to_date..params[:end_date].to_date
+    flat.bookings.each do |booking|
+      non_available_period = booking.start_date..booking.end_date
+      period.each do |day|
+        available +=1 if non_available_period.include?(day)
+        end
+      end
+    end
+    available > 0 ? true : false
+    end
 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :flats, only: [:new, :create] do
+  resources :flats, only: [:index, :new, :create] do
     resources :bookings, only: [:new, :create]
   end
 
